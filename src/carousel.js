adf07f81c0aa653c45a227230629a0f315183ced
justas.jankauskas <justas.jankauskas@visma.com>
2021-08-29 12:51:20 +0300

    implement carousel

--- src/carousel.js
+++ src/carousel.js
@@ -1,66 +1,67 @@
-
-document.querySelector('.button--hanging-left').addEventListener('click', (e) => {
-    if (activeItemIndex === 0) {
-        return;
-    }
-    activeItemIndex--;
-    carousel_content_elem.children.item(activeItemIndex + 1).classList.remove('visible');
-    carousel_content_elem.children.item(activeItemIndex).classList.add('visible');
-})
-    
-document.querySelector('.button--hanging-right').addEventListener('click', (e) => {
-    if (activeItemIndex === content_arr.length -1) {
-        return;
-    }
-    activeItemIndex++;
-    carousel_content_elem.children.item(activeItemIndex - 1).classList.remove('visible');
-    carousel_content_elem.children.item(activeItemIndex).classList.add('visible');
-});
-        
-
-let activeItemIndex = 0;
-const carousel_content_elem = document.querySelector('.carousel__content');
-const content_arr = [`/assets/jerseys/bryant-jersey.png`, `/assets/jerseys/durant-jersey.png`, `/assets/jerseys/jordan-jersey.png`, `/assets/jerseys/kyrie-jersey.png`];
+const carousel = document.querySelector(".carousel");
+const content_arr = [
+  `/assets/jerseys/bryant-jersey.png`,
+  `/assets/jerseys/durant-jersey.png`,
+  `/assets/jerseys/jordan-jersey.png`,
+  `/assets/jerseys/kyrie-jersey.png`,
+];
 
 function init() {
-    content_arr.forEach((item, index) => {
-        const img = document.createElement("IMG");
-        img.src = item;
-        img.classList.add('carousel__content__item')
-        img.style.width = '500px'
-        
-        carousel_content_elem.appendChild(img);
-    })
+  content_arr.forEach((item, index) => {
+    const div = document.createElement("div");
+    const img = document.createElement("IMG");
+    img.src = item;
+    div.classList.add("carousel__item");
 
+    if (index === 0) {
+      div.classList.add("carousel__item--visible");
+    }
+    div.appendChild(img);
+    carousel.appendChild(div);
+  });
 }
 
 init();
 
-// const carouselSlide = document.querySelector('.carousel__slide');
-// const carouselImages = document.querySelectorAll('.carousel__slide img');
+let slidePosition = 0;
+const slides = document.getElementsByClassName("carousel__item");
+const totalSlides = slides.length;
 
-// // Buttons
-// const prevBtn = document.getElementById('prevBtn');
-// const nextBtn = document.getElementById('nextBtn');
+document
+  .getElementById("carousel__button--next")
+  .addEventListener("click", () => {
+    moveToNextSlide();
+  });
 
-// // Counter
-// let counter = 1;
-// const size = carouselImages[0].clientWidth;
+document
+  .getElementById("carousel__button--prev")
+  .addEventListener("click", () => {
+    moveToPrevSlide();
+  });
 
-// carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+function updateSlidePosition() {
+  for (let slide of slides) {
+    slide.classList.remove("carousel__item--visible");
+    slide.classList.add("carousel__item--hidden");
+  }
 
-// // Button listeners
+  slides[slidePosition].classList.add("carousel__item--visible");
+}
 
-// nextBtn.addEventListener('click', () => {
-//     if(counter <= 0) return;
-//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
-//     counter++;
-//     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-// });
+function moveToNextSlide() {
+  if (slidePosition === totalSlides - 1) {
+    slidePosition = 0;
+  } else {
+    slidePosition++;
+  }
+  updateSlidePosition();
+}
 
-// prevBtn.addEventListener('click', () => {
-//     // debugger;
-//     carouselSlide.style.transition = "transform 0.4s ease-in-out";
-//     counter--;
-//     carouselSlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
-// });
+function moveToPrevSlide() {
+  if (slidePosition === 0) {
+    slidePosition = totalSlides - 1;
+  } else {
+    slidePosition--;
+  }
+  updateSlidePosition();
+}
